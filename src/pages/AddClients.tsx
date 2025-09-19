20347411eee1c113c814ff55d7e8fde6e12748ec
Laecio Filho <141887207+LaecioFillho@users.noreply.github.com>
2025-09-19 15:55:57 -0300

    feat: Clear script initial

---
+++ src/pages/AddClients.tsx
@@ -0,0 +1,13 @@
+export default function AddClients() {
+  return (
+    <>
+      <p>Digite o nome da Mesa/Cliente</p>
+      <input type="texte" />
+
+      <div>
+        <button>V</button>
+        <button>X</button>
+      </div>
+    </>
+  )
+}
---
+++ src/pages/Home.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="home-container">
+      <h1 style={{color: 'Blue'}}>Welcome to LF Cervejaria</h1>
+      <p>Your one-stop shop for the finest craft beers.</p>
+    </div>
+  );
+}
